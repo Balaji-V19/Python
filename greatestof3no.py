c3051a3cc2ab1d9a303ccc48ae5d282c1d465367
bjoffficial <50042488+bjoffficial@users.noreply.github.com>
2019-08-02 18:50:04 +0530

    Greatest among 3 numbers
    Greatest of 3 numbers

---
+++ greatestof3no.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the a value"))
+b=int(input("Enter the b value"))
+c=int(input("Enter the c value"))
+if a>b and a>c:
+    print("a is greater")
+elif b>a and b>c:
+    print("b is greater")
+else:
+    print("c is greater")
+        
