92c5b7485e649cd201affbb3d6764191f376eaec
bjoffficial <50042488+bjoffficial@users.noreply.github.com>
2019-08-03 11:06:08 +0530

    Greater than given number
    Python program to test whether all numbers of a list is greater than a certain number

---
+++ greater.py
@@ -0,0 +1,9 @@
+st=list(input("Enter the list here").split(","))
+n=int(input("Enter the number to check"))
+ln=len(st)
+for i in range(ln):
+    if int(st[i]) => n:
+        print("True")
+    else:
+        print("False")
+    
