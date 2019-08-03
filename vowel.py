9304ff5a74292b10f6a6cfe95efd0776dfbdd568
bjoffficial <50042488+bjoffficial@users.noreply.github.com>
2019-08-03 09:36:50 +0530

    Vowel
    Python program to test whether a passed letter is a vowel or not

---
+++ vowel.py
@@ -0,0 +1,11 @@
+st=input("Enter the string")
+ls=['a','A','e','E','i','I','o','O','u','U']
+if st in ls:
+    print("Vowel")
+else:
+    print("Not Vowel")
+         
+#if st=='a' or st=='A' or st=='e' or st=='E' or st=='i' or st=='I' or st=='o' or st=='O' or st=='u' or st=='U':
+#    print("Its a vowel")
+#else:
+#    print("Its not a vowel")
