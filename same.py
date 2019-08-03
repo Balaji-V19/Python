96fb7dee1205f4b6ffad67446e4d53088f9a101e
bjoffficial <50042488+bjoffficial@users.noreply.github.com>
2019-08-03 10:42:25 +0530

    If condition
    Python program that will return true if the two given integer values are equal or their sum or difference is 5

---
+++ same.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the a value"))
+b=int(input("Enter the b value"))
+if a==b:
+    print("True")
+elif a+b==5:
+    print("true")
+elif a-b==5:
+    print("True")
+else:
+    print("False")
