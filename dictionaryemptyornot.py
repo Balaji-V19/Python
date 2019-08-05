070976ce91dbe457442ddbf27f8658671ee8e17e
bjoffficial <50042488+bjoffficial@users.noreply.github.com>
2019-08-05 23:23:05 +0530

    Empty or not Dictionary
    Python program to check a dictionary is empty or not

---
+++ dictionaryemptyornot.py
@@ -0,0 +1,9 @@
+n={}
+cou=int(input("Enter the count"))
+n=[(str,input("Enter the dictonary").split())for x in range(cou)]
+print(n)
+le=len(n)
+if le==0:
+    print("Empty")
+else:
+    print("Not Empty")
