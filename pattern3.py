e597c769fbc5246d74454f8d34f75fb4241af3a6
bjoffficial <50042488+bjoffficial@users.noreply.github.com>
2019-08-06 12:00:05 +0530

    Python Pattern
    Pattern for python

--- pattern3.py
+++ pattern3.py
@@ -2,8 +2,8 @@
 count=7
 for i in range(6):
     for k in range(count):
-        print(" ",end="")
+        print(" ",end=" ")
     for j in range(i):
-        print("*",end=" ")
+        print("*",end="")
         count=count-1
     print("")    
