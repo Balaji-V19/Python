3d0efbe3614d3e1dd1700981251c88071aae17f1
bjoffficial <50042488+bjoffficial@users.noreply.github.com>
2019-08-07 22:04:00 +0530

    Circle radius and perimeter
    a Python class named Circle constructed by a radius and two methods which will compute the area and the perimeter of a circle

---
+++ circle_class.py
@@ -0,0 +1,15 @@
+from math import pi
+class circle():
+    def get_radius(self):
+        self.r=int(input("Enter the radius"))
+    def get_area(b):
+        val=pi*(b.r)**2
+        print("Area is :%.2f"%val)
+    def get_perimeter(c):
+        ans=2*pi*(c.r)
+        print("Circle's perimeter is :%.2f"%ans)
+p=circle()
+p.get_radius()
+p.get_area()
+p.get_perimeter()
+        
