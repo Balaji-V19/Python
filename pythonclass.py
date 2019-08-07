c32d715d3c6d5a704d7de8bd8cd563cc79934279
bjoffficial <50042488+bjoffficial@users.noreply.github.com>
2019-08-07 21:42:26 +0530

    Methods in python class
    a Python class which has two methods get_String and print_String. get_String accept a string from the user and print_String print the string in upper case

---
+++ pythonclass.py
@@ -0,0 +1,10 @@
+class mystr():
+    def get_string(self):
+        self.s=input("Enter the string")
+    def pr_string(abc):
+        val=abc.s
+        print(val.upper())
+p=mystr()
+p.get_string()
+p.pr_string()
+        
