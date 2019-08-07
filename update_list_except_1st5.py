1cd7d789e7c3225884674fea0e4619255e6c40b9
bjoffficial <50042488+bjoffficial@users.noreply.github.com>
2019-08-07 22:49:40 +0530

    List of values except 1st 5 using function
    Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included). Then the function needs to print all values except the first 5 elements in the list

---
+++ update_list_except_1st5.py
@@ -0,0 +1,10 @@
+def funct():
+    li=[]
+    for i in range(1,21):
+        li.append(i**2)
+    print("Function called")
+    for i in range(5,20):
+        print(li[i])
+    
+funct()    
+        
