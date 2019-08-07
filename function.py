ff55bd2ffe31cee938d77c2ea930cd04fa9d4ef1
bjoffficial <50042488+bjoffficial@users.noreply.github.com>
2019-08-07 22:11:17 +0530

    Function to print string
    a function that can convert an integer into a string and print it in the console.

---
+++ function.py
@@ -0,0 +1,10 @@
+"""def funct(strs):
+    print("String value is: ",str(strs))
+n=int(input("Enter the integer"))
+funct(n)
+
+"""
+def funct(strs):
+    print(chr(strs))
+n=int(input("Enter the integer"))
+funct(n)
